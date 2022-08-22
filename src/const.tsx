fd5fb174a843633125513192925a98131e6b76d6
Ole Morud <olekr99@hotmail.com>
2022-08-22 22:37:54 +0200

    squash

--- src/const.tsx
+++ src/const.tsx
@@ -1,5 +1,6 @@
 export const SIDEBAR_WIDTH = 8
-export const SELECTED_COLOR = "0xFF0000"
+export const SELECTED_TILE_COLOR = "Coral"
+export const DEFAULT_TILE_COLOR = "0xFFFFFF"
 export const BOARD_WIDTH = 10
 export const BOARD_HEIGHT = 10
 export const TILE_SIZE = 5
