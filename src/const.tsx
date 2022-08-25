a2cfab3d67338680770e70518703709491b93841
Ole Morud <olekr99@hotmail.com>
2022-08-25 20:45:55 +0200

    change board width

--- src/const.tsx
+++ src/const.tsx
@@ -1,7 +1,7 @@
 export const SIDEBAR_WIDTH = 8
 export const SELECTED_TILE_COLOR = "Coral"
 export const DEFAULT_TILE_COLOR = "0xFFFFFF"
-export const BOARD_WIDTH = 10
+export const BOARD_WIDTH = 20
 export const BOARD_HEIGHT = 10
 export const TILE_SIZE = 5
 export const STEP_DELAY = 300
